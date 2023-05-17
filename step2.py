5a8e2c8bdc39bcb8d75a53395a3541cc58b40e66
bennables <bensiriv@gmail.com>
2023-05-16 17:45:10 -0700

    getting text works

--- step2.py
+++ step2.py
@@ -2,28 +2,25 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
-driver = webdriver.Chrome(executable_path= 'C:/chromedriver.exe')
-driver.get("")
+driver = webdriver.Chrome()
+driver.get("https://webscraper.io/test-sites/e-commerce/allinone")
 title = driver.title
-assert title == "Web form"
+
 
 driver.implicitly_wait(1)
 
-text_box = driver.find_element(by=By.NAME, value="my-text")
-submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button")
+text_box = driver.find_elements(by=By.CLASS_NAME, value="title")
 
-text_box.send_keys("Selenium")
-print(text_box.get_attribute("value"))
-submit_button.click()
-
-
-
-message = driver.find_element(by=By.ID, value="message")
-value = message.text
-print(value)
+#get_attribute("value") is used to get the value of the input element,
+#.text is the text inside the html element
+value = text_box[0].text
+print (value)
 
 
-assert value == "Received!"
+
+
+
+
 
 print("ddd\n\n")
 
